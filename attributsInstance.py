5fa49d865c41060a69a55309ca8dce3e292f6aa2
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-11 11:58:14 +0200

    Attributs d'instance

---
+++ attributsInstance.py
@@ -0,0 +1,22 @@
+"""
+Les attributs d’instance sont des variables définies à l’aide de self. 
+Elles sont relatives à l’instance, et ne peuvent être accédées sans instanciation. 
+Dans le cadre des méthodes, ce sont les méthodes classiques d'une classe, qui possèdent  self  en premier paramètre.
+"""
+
+class Bird:
+    """Un oiseau. 🐦"""
+
+    def __init__(self):
+        """Les attributs définis ici sont des attributs d'instance."""
+        self.wings = 2
+
+    def fly(self):
+        """Cette méthode est une méthode d'instance."""
+        print("Je vole !")
+
+ 
+
+bird = Bird()  # obligation d'instancier un oiseau pour utiliser ses attributs
+bird.wings
+bird.fly()

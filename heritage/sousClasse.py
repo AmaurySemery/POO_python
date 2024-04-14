d6e0c545826b2028538c21bfe81c244b220fd285
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-14 14:53:10 +0200

    Compléments sur les sous classes

--- heritage/sousClasse.py
+++ heritage/sousClasse.py
@@ -23,6 +23,7 @@ film_cassette = FilmCassette("Blade Runner")
 
 print(film_cassette.name)
 film_cassette.watch()
+film_cassette.rewind()
 
 """
 L’héritage multiple est à utiliser avec précaution – pour la plupart des problèmes, il existe généralement des designs plus simples et plus faciles.
@@ -31,4 +32,10 @@ Si vous ne spécifiez pas de classe parent, vous dites en fait à Python de fair
 Object  constitue la base de toutes les classes, ce qui peut prêter à confusion.
 Vous vous souvenez que toutes les classes possèdent un constructeur sans argument par défaut ? 
 C’est parce qu’elles héritent d’Object.
+"""
+
+"""
+On définit une classe enfant en utilisant  class Enfant(Parent).
+Par défaut, toutes les classes héritent d’Objet – un objet Python qui fournit une fonctionnalité basique.
+Les classes peuvent hériter de classes parents multiples simultanément – dans le cas de l’héritage multiple.
 """

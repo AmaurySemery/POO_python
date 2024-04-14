1417278172b1ea11526fcf1f42005b29973defec
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-14 14:49:59 +0200

    Les sous classes

---
+++ heritage/sousClasse.py
@@ -0,0 +1,34 @@
+class Film:
+    def __init__(self, name):
+        self.name= name
+    
+    def watch(self):
+        print("Bon visionnage !")
+
+class FilmCassette(Film):
+    """Un film en cassette !"""
+
+    def __init__(self, name):
+        """Initialise le nom et la bande magnetique."""
+        self.name = name
+        self.magnetic_tape = True
+    
+    def rewind(self):
+        """Rembobine le film."""
+        print("C'est long à rembobiner !")
+        self.magnetic_tape = True
+
+film = Film("2001: l'odyssée de l'espace")
+film_cassette = FilmCassette("Blade Runner")
+
+print(film_cassette.name)
+film_cassette.watch()
+
+"""
+L’héritage multiple est à utiliser avec précaution – pour la plupart des problèmes, il existe généralement des designs plus simples et plus faciles.
+
+Si vous ne spécifiez pas de classe parent, vous dites en fait à Python de faire de la classe une sous-classe d’Object.  
+Object  constitue la base de toutes les classes, ce qui peut prêter à confusion.
+Vous vous souvenez que toutes les classes possèdent un constructeur sans argument par défaut ? 
+C’est parce qu’elles héritent d’Object.
+"""
